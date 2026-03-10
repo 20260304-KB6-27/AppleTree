0fe53ae8ab03c011b9df70ceabdbf9d93d2e44c9
Kimyerim <yeye092302@gmail.com>
2026-03-10 13:51:13 +0900

    feat: quiz 페이지 구현

---
+++ js/data.js
@@ -0,0 +1,95 @@
+const quizData = [
+    /*
+    {
+        id: ,
+        type: ,
+        question: ,
+        options: ,
+        answer:
+    }
+    */
+    {
+        id: 1,
+        type: 'multiple',
+        question: 'CPU의 구성요소가 아닌 것은?',
+        options: ['ALU', '제어장치', '레지스터', '캐시메모리'],
+        answer: 3,
+    },
+    {
+        id: 2,
+        type: 'ox',
+        question: 'HTML은 프로그래밍 언어이다.',
+        options: ['O', 'X'],
+        answer: 'X',
+    },
+    {
+        id: 3,
+        type: 'order',
+        question: '웹 개발 기본 흐름',
+        options: ['CSS', 'JavaScript', 'HTML'],
+        answer: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+        id: 4,
+        type: 'multiple',
+        question: '다음 중 컴파일 언어는?',
+        options: ['Python', 'Java', 'C', 'JavaScript'],
+        answer: 2,
+    },
+    {
+        id: 5,
+        type: 'multiple',
+        question: '다음 중 FIFO 구조는?',
+        options: ['Stack', 'Queue', 'Heap', 'Tree'],
+        answer: 1,
+    },
+    {
+        id: 6,
+        type: 'multiple',
+        question: 'IPv4의 주소 길이는?',
+        options: ['16bit', '32bit', '64bit', '128bit'],
+        answer: 1,
+    },
+    {
+        id: 7,
+        type: 'multiple',
+        question: '다음 중 버전 관리 시스템은?',
+        options: ['Git', 'MySQL', 'Docker', 'Jenkins'],
+        answer: 0,
+    },
+    {
+        id: 8,
+        type: 'ox',
+        question: 'CPU는 컴퓨터의 연산을 담당한다.',
+        options: ['O', 'X'],
+        answer: 'O',
+    },
+    {
+        id: 9,
+        type: 'ox',
+        question: 'DNS는 도메인 이름을 IP 주소로 변환한다.',
+        options: ['O', 'X'],
+        answer: 'O',
+    },
+    {
+        id: 10,
+        type: 'ox',
+        question: 'CSS는 웹페이지 스타일을 담당한다.',
+        options: ['O', 'X'],
+        answer: 'O',
+    },
+    {
+        id: 11,
+        type: 'ox',
+        question: 'HTTP는 상태를 저장하는 프로토콜이다.',
+        options: ['O', 'X'],
+        answer: 'X',
+    },
+    {
+        id: 12,
+        type: 'order',
+        question: 'HTTP 요청 흐름',
+        options: ['클라이언트 요청', '응답 반환', '서버 처리'],
+        answer: ['클라이언트 요청', '서버 처리', '응답 반환'],
+    },
+];
